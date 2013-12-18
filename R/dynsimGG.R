7c1d55cd397c08dac00032889da413ec2a4a402d
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-12-18 18:39:31 +0000

    version 0.2.1

--- R/dynsimGG.R
+++ R/dynsimGG.R
@@ -1,19 +1,22 @@
-#' Plot dynamic simulation results
+#' Plot dynamic simulation results from \code{dynsim}
 #'
-#' \code{dynsimGG} uses \code{\link{ggplot2}} to plot dynamic simulation results created by \code{\link{Dynsim}}.
+#' \code{dynsimGG} uses \code{\link{ggplot2}} to plot dynamic simulation results created by \code{\link{dynsim}}.
 #'
-#' @param obj a \code{Dynsim} class object.
+#' @param obj a \code{dynsim} class object.
 #' @param lsize size of the smoothing line. Default is 1. See \code{\link{ggplot2}}.
 #' @param color character string. Specifies the color of the lines and ribbons. If only one scenario is to be plotted then it can either be a single color value using any color value allowed by \code{\link{ggplot2}}. The default is the hexadecimal color \code{"#2B8CBE"}. If more than one scenario is to be plotted then a color brewer palette is set. The default is\code{"Set1"}. See \code{\link{scale_colour_brewer}}.
-#' @param alpha numeric. Alpha (e.g. transparency) for the points or ribbons. Default is \code{alpha = 0.1}. See \code{\link{ggplot2}}.
+#' @param alpha numeric. Alpha (e.g. transparency) for the ribbons. Default is \code{alpha = 0.1}. See \code{\link{ggplot2}}.
 #' @param xlab a label for the plot's x-axis.
 #' @param ylab a label of the plot's y-axis.
 #' @param title the plot's main title.
 #' @param leg.name name of the legend (if applicable).
 #' @param legend specifies what type of legend to include (if applicable). The default is \code{legend = "legend"}. To hide the legend use \code{legend = FALSE}. See \code{\link{discrete_scale}} for more details.
 #' @param leg.labels character vector specifying the labels for each scenario in the legend. 
+#' @param shockplot logical. If \code{TRUE} then the values of the fitted shock variable over time specified in \code{shockplot.var} will be plotted underneath the main plot. 
+#' @param shockplot.var character string naming the one shock variable to plot if \code{shockplot = TRUE}.
+#' @param shockplot.ylab a label for the shockplot's y-axis.
 #'
-#' @details Plots dynamic simulations of autoregressive relationships from \code{\link{Dynsim}}. The central line is the mean of the simulation distributions. The outer ribbon is the furthest extent of the simulation distributions' central intervals found in \code{\link{Dynsim}} with the \code{sig} argument. The middle ribbons plot the limits of the simulation distributions' central 50% intervals.
+#' @details Plots dynamic simulations of autoregressive relationships from \code{\link{dynsim}}. The central line is the mean of the simulation distributions. The outer ribbon is the furthest extent of the simulation distributions' central intervals found in \code{\link{dynsim}} with the \code{sig} argument. The middle ribbons plot the limits of the simulation distributions' central 50% intervals.
 #'
 #' @examples
 #' # Load packages
@@ -31,13 +34,13 @@
 #' M1 <- zelig(invest ~ InvestLag + mvalue + kstock, 
 #'             model = "ls", data = grunfeld, cite = FALSE)
 #'             
-#' # Set up a scenario
+#' # Set up scenarios
 #' attach(grunfeld) 
-#' Scen1 <- data.frame(invest = 317, InvestLag = mean(InvestLag, na.rm = TRUE), 
+#' Scen1 <- data.frame(InvestLag = mean(InvestLag, na.rm = TRUE), 
 #'                     mvalue = quantile(mvalue, 0.05), kstock = quantile(kstock, 0.05))
-#' Scen2 <- data.frame(invest = 100, InvestLag = mean(InvestLag, na.rm = TRUE), 
+#' Scen2 <- data.frame(InvestLag = mean(InvestLag, na.rm = TRUE), 
 #'                     mvalue = mean(mvalue), kstock = mean(kstock))
-#' Scen3 <- data.frame(invest = 317, InvestLag = mean(InvestLag, na.rm = TRUE), 
+#' Scen3 <- data.frame(InvestLag = mean(InvestLag, na.rm = TRUE), 
 #'                     mvalue = quantile(mvalue, 0.95), kstock = quantile(kstock, 0.95))
 #' detach(grunfeld)
 #'                     
@@ -45,7 +48,7 @@
 #' ScenComb <- list(Scen1, Scen2, Scen3)
 #' 
 #' ## Run dynamic simulations without shocks
-#' Sim1 <- Dynsim(obj = M1, ldv = "InvestLag", scen = ScenComb, n = 20)
+#' Sim1 <- dynsim(obj = M1, ldv = "InvestLag", scen = ScenComb, n = 20)
 #' 
 #' # Create plot legend label
 #' Labels <- c("5th Percentile", "Mean", "95th Percentile")
@@ -59,24 +62,27 @@
 #' mShocks <- data.frame(times = c(5, 10), kstock = c(100, 1000))
 #' 
 #' # Run simulations
-#' Sim2 <- Dynsim(obj = M1, ldv = "InvestLag", scen = ScenComb, n = 20,
+#' Sim2 <- dynsim(obj = M1, ldv = "InvestLag", scen = ScenComb, n = 20,
 #'                shocks = mShocks)
 #'                
 #' # Plot
 #' dynsimGG(Sim2, leg.labels = Labels)
 #'
+#' # Plot with accompanying shock plot
+#' dynsimGG(Sim2, leg.labels = Labels, shockplot.var = "kstock")
+#'
 #' @import ggplot2
-#' @importFrom gridExtra
+#' @importFrom gridExtra grid.arrange
 #'
 #' @export
 
-dynsimGG <- function(obj, lsize = 1, color = NULL, alpha = 0.5, xlab = "\nTime", ylab = "Predicted Value\n", title = NULL, leg.name = "Scenario", leg.labels = NULL, legend = "legend"){
+dynsimGG <- function(obj, lsize = 1, color = NULL, alpha = 0.5, xlab = "\nTime", ylab = "Predicted Value\n", title = NULL, leg.name = "Scenario", leg.labels = NULL, legend = "legend", shockplot = FALSE, shockplot.var = NULL, shockplot.ylab = NULL){
 	# CRAN requirements
-	ldvMean <- ldvLower <- ldvUpper <- ldvLower50 <- ldvUpper50 <- scenNumber <- NULL
+	ldvMean <- ldvLower <- ldvUpper <- ldvLower50 <- ldvUpper50 <- scenNumber <- shockvar <- NULL
 
-	# Check if obj is of the Dynsim class
-	if (class(obj) != "Dynsim"){
-		stop("obj must be a Dynsim class object.")
+	# Check if obj is of the dynsim class
+	if (class(obj) != "dynsim"){
+		stop("obj must be a dynsim class object.")
 	}
 	# Reclass obj as a data frame for ggplot2
 	class(obj) <- "data.frame"
@@ -90,20 +96,54 @@ dynsimGG <- function(obj, lsize = 1, color = NULL, alpha = 0.5, xlab = "\nTime",
 		if (is.null(color)){
 			color <- "#2B8CBE" 
 		}	
-		OutPlot <- ggplot(obj, aes(time, ldvMean)) +
+		MainPlot <- ggplot(obj, aes(time, ldvMean)) +
 					geom_line(size = lsize, colour = color) +
 					geom_ribbon(aes(ymin = ldvLower, ymax = ldvUpper), alpha = alpha, fill = color, linetype = 0) +
 					geom_ribbon(aes(ymin = ldvLower50, ymax = ldvUpper50), alpha = alpha, fill = color, linetype = 0) +
 			        xlab(xlab) + ylab(ylab) +
 			        ggtitle(title) +
 			        theme_bw(base_size = 15)
+		# Add shock fitted value plot
+		if (isTRUE(shockplot)){
+		  if (is.null(shockplot.var) | length(shockplot.var) > 1){
+		    stop("You must specify ONE shock variable to plot with the shockplot.var argument.")
+		  }
+		  if (is.null(shockplot.ylab)){
+		  	shockplot.ylab <- paste0(shockplot.var, "\n")
+		  }
+		  shockvar.pos <- paste0("shock.", shockplot.var)
+		  shockvar.pos <- match(shockvar.pos, names(obj))
+		  if(is.null(shockvar.pos)){
+		    stop(paste(shockplot.var, "was not used as a shock variable."))
+		  }
+		  shockplot.df <- obj[, c(1, shockvar.pos)]
+		  names(shockplot.df) <- c("time", "shockvar")
+		  
+		  ShockPlot <- ggplot(shockplot.df, aes(time, shockvar)) + 
+		    geom_line(colour = color) +
+		    ylab(shockplot.ylab) + xlab("") +
+		    theme_bw(base_size = 15)
+		  
+		  gA <- ggplotGrob(MainPlot)
+		  gB <- ggplotGrob(ShockPlot)
+		  
+		  # Set equal widths
+		  maxWidth = grid::unit.pmax(gA$widths[2:5], gB$widths[2:5])
+		  gA$widths[2:5] <- as.list(maxWidth)
+		  gB$widths[2:5] <- as.list(maxWidth)
+		  
+		  grid.arrange(gA, gB, ncol = 1, heights = c(4, 1))
+		}
+		else if (!isTRUE(shockplot)){
+		  MainPlot
+		}
 	}
 	# Plot multiple scenarios
 	else if (isTRUE("scenNumber" %in% names(obj))){
 		if (is.null(color)){
 			color <- "Set1" 
 		}	
-		OutPlot <- ggplot(obj, aes(time, ldvMean, colour = factor(scenNumber), fill = factor(scenNumber))) +
+		MainPlot <- ggplot(obj, aes(time, ldvMean, colour = factor(scenNumber), fill = factor(scenNumber))) +
 					geom_line(size = lsize) +
 					geom_ribbon(aes(ymin = ldvLower, ymax = ldvUpper), alpha = alpha, linetype = 0) +
 					geom_ribbon(aes(ymin = ldvLower50, ymax = ldvUpper50), alpha = alpha, linetype = 0) +
@@ -114,6 +154,41 @@ dynsimGG <- function(obj, lsize = 1, color = NULL, alpha = 0.5, xlab = "\nTime",
 			        xlab(xlab) + ylab(ylab) +
 			        ggtitle(title) +
 			        theme_bw(base_size = 15)
+	
+    # Add shock fitted value plot
+    if (isTRUE(shockplot)){
+      if (is.null(shockplot.var) | length(shockplot.var) > 1){
+        stop("You must specify ONE shock variable to plot with the shockplot.var argument.")
+      }
+	  if (is.null(shockplot.ylab)){
+	  	shockplot.ylab <- paste0(shockplot.var, "\n")
+	  }
+      shockvar.pos <- paste0("shock.", shockplot.var)
+      shockvar.pos <- match(shockvar.pos, names(obj))
+      if(is.null(shockvar.pos)){
+        stop(paste(shockplot.var, "was not used as a shock variable."))
+      } else
+      shockplot.df <- obj[, c(1:2, shockvar.pos)]
+      names(shockplot.df) <- c("scenNumber", "time", "shockvar")
+      
+      ShockPlot <- ggplot(shockplot.df, aes(time, shockvar, colour = as.factor(scenNumber))) + 
+        geom_line() +
+        scale_colour_brewer(palette = color, guide = FALSE) +
+        ylab(shockplot.ylab) + xlab("") +
+        theme_bw(base_size = 15)
+      
+      gA <- ggplotGrob(MainPlot)
+      gB <- ggplotGrob(ShockPlot)
+      
+      # Set equal widths
+      maxWidth = grid::unit.pmax(gA$widths[2:5], gB$widths[2:5])
+      gA$widths[2:5] <- as.list(maxWidth)
+      gB$widths[2:5] <- as.list(maxWidth)
+      
+      grid.arrange(gA, gB, ncol = 1, heights = c(4, 1))
+    }
+    else if (!isTRUE(shockplot)){
+      MainPlot
+    }
 	}
-  OutPlot
 }
--- R/utils.R
+++ R/utils.R
@@ -11,17 +11,17 @@
 #' @keywords internals
 #' @noRd
 
-OneScen <- function(obj, ldv, n, scen, sig, shocks = NULL){
+OneScen <- function(obj, ldv, n, scen, sig, num, shocks, forecast){
 	# CRAN requirements
-	times <- NULL
+	times <- sigma.sqr <- alpha.sqr <- NULL
 	
-	# Create lower and upper bounds of the confidence interval
+	# Create lower and upper percentile bounds of the confidence interval
 	Bottom <- (1 - sig)/2
 	Top <- 1 - Bottom	
 
 	# Create data frame to fill in with simulation summaries
 	SimSum <- data.frame()
-  ShockVals <- data.frame()
+	ShockVals <- data.frame()
 
 	# Change data frame for shock values
 	for (i in 1:n){
@@ -40,19 +40,38 @@ OneScen <- function(obj, ldv, n, scen, sig, shocks = NULL){
 				scenTemp <- scen
 			}
 		}
+    
+		# Add in dependent variable for setx
+		DV <- as.character(obj$formula[2])
+		DVmean <- data.frame(mean(obj$data[, DV]))
+		names(DVmean) <- DV
+		scenTemp <- cbind(DVmean, scenTemp)
 
 		# Run simulations
 		SetVales <- setx(obj = obj, data = scenTemp)
-		SimValues <- sim(obj = obj, x = SetVales)
+		SimValues <- sim(obj = obj, x = SetVales, num = num)
 
 		# Create summary data frame
 		PV <- simulation.matrix(SimValues, "Predicted Values: Y|X")
 		time <- i
 		ldvMean <- mean(PV)
-		ldvLower <- quantile(PV, prob = Bottom, names = FALSE)
-		ldvUpper <- quantile(PV, prob = Top, names = FALSE)
-		ldvLower50 <- quantile(PV, prob = 0.25, names = FALSE)
-		ldvUpper50 <-quantile(PV, prob = 0.75, names = FALSE)
+    
+    if (is.null(forecast)){
+		  ldvLower <- quantile(PV, prob = Bottom, names = FALSE)
+		  ldvUpper <- quantile(PV, prob = Top, names = FALSE)
+		  ldvLower50 <- quantile(PV, prob = 0.25, names = FALSE)
+		  ldvUpper50 <-quantile(PV, prob = 0.75, names = FALSE)
+    }
+    else if (!is.null(forecast)){
+      sigma.sqrDF <- 
+      alpha.sqrDF <- 
+      if (forecast == "ag"){
+
+        iMinusOne <- i - 1
+        se <- sqrt(sigma.sqr *  (1 * iMinusOne * sigma.sqr * alpha.sqr)) 
+        
+      }
+    }
     
     # Shock variable values
 	if (!is.null(shocks)){
